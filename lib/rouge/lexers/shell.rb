14b2709ddec5f0ed4a39fc7968e323de1d27e4ae
Gabor Szarnyas <szarnyasg@gmail.com>
2024-03-02 16:03:09 +0100

    Shell: Add Python package managers

--- lib/rouge/lexers/shell.rb
+++ lib/rouge/lexers/shell.rb
@@ -44,7 +44,7 @@ module Rouge
         runcon chroot env nice nohup stdbuf timeout kill sleep factor numfmt
         seq tar grep sudo awk sed gzip gunzip apt apt-get yum apk add brew
         upgrade update python python3 pip pip3 unzip duckdb npm cargo go get
-        winget
+        winget conda mamba
       ).join('|')
 
       state :basic do
