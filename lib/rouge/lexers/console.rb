f8201921927e0c76c6ff502004c4d8113a6b4f28
Gabor Szarnyas <szarnyasg@gmail.com>
2024-04-25 08:44:26 +0200

    Add some code to ConsoleLexer based on PlainText (plain_text)

--- lib/rouge/lexers/console.rb
+++ lib/rouge/lexers/console.rb
@@ -4,6 +4,20 @@
 module Rouge
   module Lexers
     class ConsoleLexer < Lexer
+      tag 'console'
+      aliases 'terminal', 'shell_session', 'shell-session'
+      desc 'A generic lexer for error messages in the DuckDB documentation'
+
+      attr_reader :token
+      def initialize(*)
+        super
+
+        @token = token_option(:token) || Text
+      end
+
+      def stream_tokens(string, &b)
+        yield self.token, string
+      end
     end
   end
 end
