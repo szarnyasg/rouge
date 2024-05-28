686f140a0e4d11438f50687dba93a3d9b082dbf6
Gabor Szarnyas <szarnyasg@gmail.com>
2024-05-28 09:45:50 +0200

    Allow lowercase 'e' for E notation

--- lib/rouge/lexers/sql.rb
+++ lib/rouge/lexers/sql.rb
@@ -1792,8 +1792,8 @@ module Rouge
         rule %r/"/, Name::Variable, :double_string
         rule %r/`/, Name::Variable, :backtick
         # Numbers
-        rule %r/-?\d[\d_]*\.\d[\d_]*(E-?\d[\d_]*(\.\d[\d_]*)?)?/, Num::Float
-        rule %r/-?\d[\d_]*(E-?\d[\d_]*)?/, Num::Integer
+        rule %r/-?\d[\d_]*\.\d[\d_]*([Ee]-?\d[\d_]*(\.\d[\d_]*)?)?/, Num::Float
+        rule %r/-?\d[\d_]*([Ee]-?\d[\d_]*)?/, Num::Integer
 
         # Strings 'something(' are candidates to be treated as function names
         rule %r/(\w[\w\d]*)(\()/ do |m|
