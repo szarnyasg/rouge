6113023afe4ac03b1eefe5daee1eb8f2b9a8ce05
Gabor Szarnyas <szarnyasg@gmail.com>
2024-08-21 11:22:47 +0200

    Add shell command

--- lib/rouge/lexers/shell.rb
+++ lib/rouge/lexers/shell.rb
@@ -46,7 +46,7 @@ module Rouge
         upgrade update python python3 pip pip3 unzip duckdb npm cargo go get
         winget conda mamba make ninja cmake git g\+\+ ninja-build
         libssl-dev openssl-devel wget curl aws tree uplot jq tldr pcregrep
-        isql clone hexdump
+        isql clone hexdump iconv
       ).join('|')
 
       state :basic do
